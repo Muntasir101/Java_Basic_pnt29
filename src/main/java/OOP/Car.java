0e20eed2382895a6a4f5357495320e0986cb86ad
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-01-22 15:58:12 +0600

    OOP

---
+++ src/main/java/OOP/Car.java
@@ -0,0 +1,36 @@
+package OOP;
+
+public class Car {
+    static String Car_name;
+    static String Car_model;
+
+    void start_car() {
+        System.out.println(Car_name + " Start..");
+    }
+
+    void stop_car() {
+        System.out.println(Car_name+ " Stop..");
+    }
+
+    public Car(String Car_name, String Car_model){
+        this.Car_name = Car_name;
+        this.Car_model = Car_model;
+    }
+
+    public static void main(String[] args) {
+        Car BMW = new Car("BMW", "B-2024");
+        //System.out.println("Car Name: "+ Car_name+" Model: "+ Car_model);
+        BMW.start_car();
+        BMW.stop_car();
+
+        Car Toyota = new Car("Toyota","T2024");
+        //System.out.println("Car Name: "+ Car_name+" Model: "+ Car_model);
+        Toyota.start_car();
+        Toyota.stop_car();
+
+        Car Suzuki = new Car("Suzuki","S2024");
+        //System.out.println("Car Name: "+ Car_name+" Model: "+ Car_model);
+        Suzuki.start_car();
+        Suzuki.stop_car();
+    }
+}
